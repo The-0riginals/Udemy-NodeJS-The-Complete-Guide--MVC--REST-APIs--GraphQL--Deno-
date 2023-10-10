962e075665a0ecdc8dcbd8e766f84b97a98c4bb2
Klaus <whynotmemylove99@gmail.com>
2023-10-10 19:50:19 +0700

    playing around with Ejs: lectures 91 to 93

---
+++ 06.Working with Dynamic Content&Adding Templating Engines/lecture91/app.js
@@ -0,0 +1,45 @@
+// Installing & Implementing Pug
+// npm install --save pug
+// https://pugjs.org/api/getting-started.html
+
+const path = require('path');
+
+const express = require('express');
+const expressHbs = require("express-handlebars");
+
+const app = express();
+
+// app.engine("hbs", expressHbs.engine({
+//     layoutsDir: "views/layouts",
+//     defaultLayout: "main-layout",
+//     extname: "hbs"
+// }));
+
+// view engine helps express know which template engine to use
+//app.set('view engine','pug');// set the template engine to pug
+//app.set("view engine", "hbs");// set the template engine to handlebars
+app.set("view engine", "ejs");// set the template engine to ejs
+
+//views helps express know where to find the templates
+app.set('views','views');// set the views folder to views, default is views
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+// register a parser for incoming requests that have a JSON body
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/admin',adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    //res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    res.status(404).render('404',{
+        pageTitle: 'Page Not Found',
+        path: req.url,
+        layout: false});
+});
+console.log('Listening on port 3000');
+
+app.listen(3000);
---
+++ 06.Working with Dynamic Content&Adding Templating Engines/lecture91/routes/admin.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+    //console.log('In another middleware!');
+    //send allows us to send a response to the client
+    //res.sendFile(path.join(rootDir,'views','add-product.html'));
+    res.render('add-product',{
+        pageTitle: 'Add Product', 
+        path: '/admin/add-product', 
+        formsCSS: true, 
+        productCSS: true, 
+        activeAddProduct: true});
+});
+
+router.post('/add-product', (req, res, next) => {
+    //console.log(req.body);
+    products.push({title: req.body.title});
+    res.redirect('/');
+});
+
+//module.exports = router;
+
+exports.routes = router;
+exports.products = products;
