970588c934750599a5d512a8ecd1d3b7497b3422
Klaus <whynotmemylove99@gmail.com>
2023-10-17 22:31:27 +0700

    add lectures 123 to 127: add cart,edit product

---
+++ 09. Dynamic Routes & Advanced Models/lecture123/models/cart.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
+const p=path.join(
+    path.dirname(require.main.filename),
+    'data',
+    'cart.json'
+);
+
+module.exports = class Cart {
+    static addProduct(id, productPrice){
+        //Fetch the previous cart
+        fs.readFile(p, (err, fileContent) => {
+            let cart = {products: [], totalPrice: 0};
+            if(!err){
+                cart = JSON.parse(fileContent);
+            }
+            //Analyze the cart => Find existing product
+            //if the product is already in the cart, we will get the index of the product
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);//findIndex() is a JS function that takes a function as an argument and that function will be executed on every element in the array and it will return true if the element fulfills the condition and false if it doesn't
+            const existingProduct = cart.products[existingProductIndex];//if the product is already in the cart, we will get the product
+            let updatedProduct;
+
+            //Add new product/ increase quantity
+            //here we are not modifying orignal array because we might be using refrence of this array somewhere else and changing this refrence may change other things in our app.
+            if(existingProduct){//if the product is already in the cart, we will update the quantity of the product
+                updatedProduct = {...existingProduct};
+                updatedProduct.qty = updatedProduct.qty + 1;
+                cart.products = [...cart.products];//ensure that this array can't (accidentally) be mutated from some other place in the app
+                cart.products[existingProductIndex]=updatedProduct; //replace the existing product with the updated product
+            }
+            else{
+                updatedProduct = {id: id, qty: 1};
+                cart.products = [...cart.products, updatedProduct]; //add the updated product to the cart
+            }
+
+            cart.totalPrice = cart.totalPrice + +productPrice;//convert productPrice to a number by adding a plus sign in front of it
+            fs.writeFile(p, JSON.stringify(cart), err => {
+                console.log(err);
+            });
+        }
+        );
+    }
+
+}
---
+++ 09. Dynamic Routes & Advanced Models/lecture123/routes/admin.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+const express = require('express');
+
+const adminController = require('../controllers/admin');
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
+
+// admin/products => GET
+router.get('/products', adminController.getProducts);
+
+router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+module.exports = router; //  sets the entire module.exports object to router
+//This means that when you import this module in another file, you'll get the router object directly.
+//adminRoutes will be the router object.
+
+//exports.routes = router; // This statement adds a property called routes to the exports object.
+//exports.products = products;
+//this means 'adminRoutes.routes' will be the router object.
+
+// we need to use module.exports instead of exports because exports is just a pointer to module.exports
+// and we are changing the value of exports which is not possible
