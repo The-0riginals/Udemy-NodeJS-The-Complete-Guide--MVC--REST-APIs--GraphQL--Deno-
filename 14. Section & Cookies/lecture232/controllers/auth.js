517710502c8c5ad75ad539de60af87ebaa48b489
Klaus <whynotmemylove99@gmail.com>
2023-11-22 05:23:02 +0700

    add to lecture 235

--- 14. Section & Cookies/lecture232/controllers/auth.js
+++ 14. Section & Cookies/lecture232/controllers/auth.js
@@ -1,7 +1,15 @@
 exports.getLogin = (req, res, next) => {
+    //const isLoggedIn = req.get('Cookie').split(';')[1].trim().split('=')[1] === 'true';//this is the old way to get the cookie
     res.render('auth/login', {
         pageTitle: 'Login',
         path: '/login',
         isAuthenticated: false
     });
 };
+
+exports.postLogin = (req, res, next) => {
+    //req.isLoggedIn = true;
+    res.setHeader('Set-Cookie','loggedIn=true');// cookie helps us to store data on the client side
+    //res.setHeader('Set-Cookie','loggedIn=true; Max-Age=10');// Max-Age is the time in seconds that the cookie will be stored
+    res.redirect('/');
+}
