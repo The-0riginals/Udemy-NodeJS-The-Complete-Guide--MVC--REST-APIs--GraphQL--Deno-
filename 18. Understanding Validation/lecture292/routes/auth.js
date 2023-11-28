364259156d60be5f31bdbaf9e852b07a89c7e116
Klaus <whynotmemylove99@gmail.com>
2023-11-28 16:19:11 +0700

    add new section :valid stuff

---
+++ 18. Understanding Validation/lecture292/routes/auth.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const { check, body } = require('express-validator');//this is a function that returns a middleware
+
+const router = express.Router();
+
+const authController = require('../controllers/auth');
+const User = require('../models/user');
+
+router.get('/login', authController.getLogin);
+
+router.post('/login', authController.postLogin);
+
+router.post('/logout', authController.postLogout);
+
+router.get('/signup', authController.getSignup);
+
+router.post('/signup',
+    [
+        check('email')
+            .isEmail()
+            .withMessage('please enter a valid email')
+            .custom((value, {req}) => {
+                // if(value === 'test@test.com'){
+                //     throw new Error('forbidden email.');
+                // }
+                // return true;//this is important to return true if the validation succeeds
+                return User.findOne({email: value})
+                    .then(userDoc => {
+                        if(userDoc){
+                            return Promise.reject('Email already exists, please pick a different one');// promise.reject() is a function provided by mongoose
+                        }
+                    }
+                );
+            }
+        ),
+        body('password', 'Please enter a password with only numbers and text and at least 5 characters.')//this is a custom error message for every validation
+            .isLength({min: 5})
+            .isAlphanumeric(), //only letters and numbers
+        body('confirmPassword')//'confirmPassword' is the name of the input field
+            .custom((value, {req}) => {
+                if(value !== req.body.password){
+                    throw new Error('Passwords have to match!');
+                }
+                return true;//this is important to return true if the validation succeeds
+            })
+
+    ],
+    authController.postSignup
+);
+
+router.get('/reset', authController.getReset);
+
+router.post('/reset', authController.postReset);
+
+router.get('/reset/:token', authController.getNewPassword);
+
+router.post('/new-password', authController.postNewPassword);
+
+module.exports = router;
